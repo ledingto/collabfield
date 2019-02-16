e1a25584df128f7d0a41520525456694dd94aacf
Laura Edington <edington.laura@gmail.com>
2019-02-15 20:30:33 -0800

    Route devise sign-in page to /login

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#index'
+
+  # route devise sign-in page to /login in addition to /users/sign_in
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
 end
