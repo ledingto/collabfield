88b6038d1c47e54169c604915d4500b3f784effa
Laura Edington <edington.laura@gmail.com>
2019-02-20 20:05:12 -0800

    Route devise sign-up page to /signup

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
+
+  # route devise sign-in page to /login in addition to /users/sign_in
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
 end
