d473671e28096e89c381b0a3c6e681ad49f55fdd
Laura Edington <edington.laura@gmail.com>
2019-02-26 08:55:24 -0800

    Configure navigation helpers; extract logic from view into partials

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Collabfield
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Override default behavior to load all helper files to all views
+    # Now helpers are available for corresponding controller's views only
+    # (e.g. pages_helper.rb and PagesController)
+    config.action_controller.include_all_helpers = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
